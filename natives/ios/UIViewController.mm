8e6034ceb24a8d344d825c28ac7b999efb9f1d6a
Furo <furogaming9@gmail.com>
2025-04-07 10:42:57 +0200

    idk

---
+++ natives/ios/UIViewController.mm
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+@end
+
+@implementation MainViewController
+
+- (BOOL)prefersHomeIndicatorAutoHidden {
+    return YES;
+}
+
+@end
